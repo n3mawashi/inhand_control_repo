b9bc77924825480ad1f57610eea87b87d4bbe99c
ptiggerdine <ptiggerdine@users.noreply.github.com>
2021-12-31 18:57:54 +1000

    change packer option to single - for inhand script

--- packer/build_windows_2019_hyperv_inhand.ps1
+++ packer/build_windows_2019_hyperv_inhand.ps1
@@ -3,7 +3,7 @@ if (Test-Path ..\..\output-hyperv-iso) {
 }
 
 packer build --only=hyperv-iso `
-  --var hyperv_switchname=mgmt
-  --var iso_url=..\..\ISOs\2019_SERVER_EVAL_x64FRE_en-us.iso `
-  --var iso_checksum="none" `
+  -var hyperv_switchname=mgmt
+  -var iso_url=..\..\ISOs\2019_SERVER_EVAL_x64FRE_en-us.iso `
+  -var iso_checksum="none" `
   windows_2019_hyperv_inhand.json
