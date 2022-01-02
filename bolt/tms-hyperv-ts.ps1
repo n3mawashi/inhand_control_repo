8fcbc7350a06a245f22517aafd251bd806a2ca5a
ptiggerdine <ptiggerdine@users.noreply.github.com>
2022-01-03 01:46:57 +1000

    Fix output_dir round2 and init script addition

--- bolt/tms-hyperv-ts.ps1
+++ bolt/tms-hyperv-ts.ps1
@@ -8,16 +8,36 @@ TODO:
 3. take over hte world
 #>
 iso_dir = D:\ISO\
-packer_output_dir = D:\images\
+packer_output_dir = D:\Templates\windows_2019_hyperv-iso
 
 # 1. run bolt tasks/plan to confirm hyper is install/setup as we expect
-bolt task run hyperv::install --target hvserver
-bolt task run hyperv::configure --target hvserver
+bolt task run windows::hyperv_install --target hvserver
+bolt task run windows::hyperv_configure --target hvserver
 
 # 2. Create image with installation and inital configuration done
+# Gen 2 impage needs an additional step
+#./packer/make_unattend_iso.ps1
 
+if (-not (test-item -path $packer_out_dir)) {
+  emove-Item -Recurse -Force $packer_out_dir
+}
+#packer validate
+packer build --only=hyperv-iso `
+  -var hyperv_switchname=mgmt `
+  -var iso_url=..\..\ISOs\2019_SERVER_EVAL_x64FRE_en-us.iso `
+  -var iso_checksum='none' `
+  -var output_directory=$packer_output_dir `
+  windows_2019_hyperv_inhand.json
 
 # 3. Run terraform apply to create VM and import packer image.
+terraform init
+terraform plan `
+  -var vswitch_name="mgmt" `
+  -var ts_hostname="TMS-S-RDS01" `
+  -var vmpath="D:\\Hyper-V\\Virtual Machines\\" `
+  -var vmtemplate="D:\\Templates\\windows_2019_hyperv.box" `
+  -var ts_ram=""
+
 # 3.a because terraform
 # 4. Run bolt to configure RDS server further.
 
--- packer/build_windows_2019_hyperv_inhand.ps1
+++ packer/build_windows_2019_hyperv_inhand.ps1
@@ -1,9 +1,12 @@
-if (Test-Path ..\..\output-hyperv-iso) {
-  Remove-Item -Recurse -Force ..\..\output-hyperv-iso
+$output_dir = 'D:\Templates\windows_2019_hyperv-iso'
+
+if (Test-Path $output_dir) {
+  Remove-Item -Recurse -Force $output_dir
 }
 
 packer build --only=hyperv-iso `
   -var hyperv_switchname=mgmt `
   -var iso_url=..\..\ISOs\2019_SERVER_EVAL_x64FRE_en-us.iso `
   -var iso_checksum='none' `
+  -var output_directory=$output_dir
   windows_2019_hyperv_inhand.json
